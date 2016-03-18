b12e68c1f9b24ff09bd1a5d35ea462291fd1628f
pettorin <valeria.pettorino@me.com>
2016-03-18 09:07:43 +0100

    test

--- test.py
+++ test.py
@@ -61,7 +61,49 @@ for line in fh:
     medical_medcode.append(medical_fields[4])
 
 "printing the patient fields we want"    
-print medical_medcode
+#print medical_medcode
+
+
+" ----- AHD FILE -------- "
+filename = 'Ahd_a0001.txt' 
+sub_dir = '/Users/pettorin/Dropbox/S2DS/selfcare/s2ds/PrimarySelfCare/primarycaredata/THINData'
+fh = open(os.path.join(sub_dir, filename), "r")
+
+"Input values:"
+#Patient file input values
+ahd_parts = (4,8,10,1,13,13,13,13,13,13,7,1,3,2,4,7,1,1,1,1,4,4,8,1) # lengths of the parts for this file
+ahd_parts_number = len(ahd_parts) # how many parts there are
+
+"Patient fields we want:"
+ahd_patid = list()
+ahd_ahdcode = list()
+ahd_data1 = list()
+ahd_data2 = list()
+ahd_data3 = list()
+ahd_data4 = list()
+ahd_data5 = list()
+ahd_data6 = list()
+ahd_medcode = list()
+
+"lines splitted and appended to a list: filling the patient fields we want"
+count = 0
+for line in fh:
+#    print line        
+    ahd_fields = [line[int(sum(ahd_parts[:i])):sum(ahd_parts[:i+1])].strip() for i in range(len(ahd_parts))]
+#    print medical_fields
+    ahd_patid.append(ahd_fields[0])
+    ahd_ahdcode.append(ahd_fields[2])   
+    ahd_data1.append(ahd_fields[4])       
+    ahd_data2.append(ahd_fields[5])           
+    ahd_data3.append(ahd_fields[6])       
+    ahd_data4.append(ahd_fields[7])       
+    ahd_data5.append(ahd_fields[8])       
+    ahd_data6.append(ahd_fields[9])       
+    ahd_medcode.append(ahd_fields[10])
+
+"printing the patient fields we want"    
+print ahd_ahdcode
+
 
     
 
